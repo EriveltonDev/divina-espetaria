efc129c674b83774250380f49ce7c76dc57954ba
EriveltonDev <eriveltondesousa26@gmail.com>
2025-01-05 09:29:33 -0300

    fix: menu mobile

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -3,18 +3,33 @@
 import Image from "next/image";
 import styles from './style.module.css'
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMedia } from "../../hooks/useMedia";
 
 export function Header() {
   const [mobileIsOpen, setMobileIsOpen] = useState(false)
   const isMobile = useMedia('(max-width: 1042px)')
+  const menuMobileRef = useRef<HTMLUListElement | null>(null)
 
   useEffect(() => {
     if(!isMobile) {
       setMobileIsOpen(false)
     }
   }, [isMobile])
+
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (mobileIsOpen && menuMobileRef.current && !menuMobileRef.current.contains(event.target as Node)) {
+        setMobileIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [mobileIsOpen]);
 
   return (
     <header className={styles.header}>
@@ -34,51 +49,51 @@ export function Header() {
           <span className={styles.bar}></span>
         </div>
 
-        <ul className={`${styles.ul} ${mobileIsOpen ? styles.active : undefined}`}>
+        <ul ref={menuMobileRef} className={`${styles.ul} ${mobileIsOpen ? styles.active : undefined}`}>
           <li className={styles.li}>
-            <Link className={styles.nav__link} href="">
+            <Link onClick={() => setMobileIsOpen(false)} className={styles.nav__link} href="">
               Lojas
             </Link>
           </li>
 
           <li className={styles.li}>
-            <Link className={styles.nav__link} href="">
+            <Link onClick={() => setMobileIsOpen(false)} className={styles.nav__link} href="">
               Preços
             </Link>
           </li>
 
           <li className={styles.li}>
-            <Link className={styles.nav__link} href="">
+            <Link onClick={() => setMobileIsOpen(false)} className={styles.nav__link} href="">
               Ofertas
             </Link>
           </li>
 
           <li className={styles.li}>
-            <Link className={styles.nav__link} href="">
+            <Link onClick={() => setMobileIsOpen(false)} className={styles.nav__link} href="">
               Kit Churrasco
             </Link>
           </li>
 
           <li className={styles.li}>
-            <Link className={styles.nav__link} href="">
+            <Link onClick={() => setMobileIsOpen(false)} className={styles.nav__link} href="">
               Churrasqueiras
             </Link>
           </li>
 
           <li className={styles.li}>
-            <Link className={styles.nav__link} href="">
+            <Link onClick={() => setMobileIsOpen(false)} className={styles.nav__link} href="">
               Revenda
             </Link>
           </li>
 
           <li className={styles.li}>
-            <Link className={styles.nav__link} href="">
+            <Link onClick={() => setMobileIsOpen(false)} className={styles.nav__link} href="">
               Artigos
             </Link>
           </li>
 
           <li className={styles.li}>
-            <Link className={styles.nav__link} href="">
+            <Link onClick={() => setMobileIsOpen(false)} className={styles.nav__link} href="">
               Distribuidora
             </Link>
           </li>
